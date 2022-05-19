8d6adb948def0072e32e5c670a625dd34af593ed
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2022-05-19 09:19:39 +0530

    V05.19

---
+++ script/customStript/navbar.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $('#trainmap').click(function() {
+        window.location.replace('trainmap.html');
+    });
+    $('#trainlist').click(function() {
+        window.location.replace('trainlist.html');
+    });
+    // $('#trainsearch').click(function() {
+    //     window.location.replace('trainmap.html');
+    // });
+    $('#pricecal').click(function() {
+        window.location.replace('costcal.html');
+    });
+    $('#seatbooking').click(function() {
+        window.location.replace('booking.html');
+    });
+    $('#gallery').click(function() {
+        window.location.replace('gallery.html');
+    });
+    // $('#contact').click(function() {
+    //     window.location.replace('trainmap.html');
+    // });
+    $('#about').click(function() {
+        window.location.replace('history.html');
+    });
+    $('#login').click(function() {
+        window.location.replace('login.html');
+    });
+
+});
